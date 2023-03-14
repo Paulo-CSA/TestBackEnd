00662cf812f66b4b1b0a3a46b695f733ff6539c3
Paulo Cesar <paulo.csa@outlook.com>
2023-03-14 17:04:28 -0300

    atualização de novo metodo

---
+++ Calculadora/XUnitTest/CalculadoraIMC.cs
@@ -0,0 +1,38 @@
+﻿using Calculadora;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XUnitTest
+{
+    public class CalculadoraIMC
+    {
+        [Fact]
+        public void CalcularIMC() 
+        {
+            //preparação
+            double peso = 110;
+            double altura = 1.79;
+
+            //ação
+            var resultado = operacoes.calcularIMC(peso, altura);
+
+            //verificação
+            Assert.Equal(34.33, resultado);
+        }
+        [Theory]
+        [InlineData(80,1.79,24.97)]
+        [InlineData(100, 1.79, 31.21)]
+        
+        public void CalcularIMC2(double peso, double altura, double ResultadoEsp)
+        { 
+            //ação
+            var resultadoIMC = operacoes.calcularIMC(peso, altura);
+
+            //verificação
+            Assert.Equal(ResultadoEsp, resultadoIMC);
+        }
+    }
+}
---
+++ Calculadora/XUnitTest/ClassificarIMC.cs
@@ -0,0 +1,39 @@
+﻿using Calculadora;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XUnitTest
+{
+    
+    public class ClassificarIMC
+    {
+        [Fact]
+        public void ClassificaIMC() 
+        {
+            //preparação
+            double imc = 28;
+
+            //ação
+            var resultado = operacoes.classificarIMC(imc);
+
+            //verificação
+            Assert.Equal("Sobrepeso", resultado);
+
+        }
+        [Theory]
+        [InlineData(28, "Sobrepeso")]
+        [InlineData(31, "Obesidade Grau I")]
+        public void classificaResultadoIMC(double imc, string resultadoEsperado)
+        {
+            //ação
+            var resultado = operacoes.classificarIMC(imc);
+
+            //verificação
+            Assert.Equal(resultadoEsperado, resultado);
+        }
+        
+    }
+}
